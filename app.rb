b01f687a6f8fd8e10ec947ff67a08fdc0d7ae70e
Chris Clifford <cclifford82@gmail.com>
2016-12-09 11:48:38 -0800

    add initial html templates

--- app.rb
+++ app.rb
@@ -9,5 +9,11 @@ require('pry')
 DB = PG.connect({:dbname => "hair_salon"})
 
 get('/') do
+  @stylists = Stylist.all()
   erb(:index)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
 end
